b9efacdddc89ca768f3bce83cb907ebf5a7b9c2c
Dmitriy <svardmitriy@gmail.com>
2019-07-20 11:26:50 +0600

    changed timer

--- js2/task06.js
+++ js2/task06.js
@@ -1,13 +1,21 @@
-
-
 function getTi() {
     var str = "";
     var timer = new Date();
+    var hours = timer.getHours();
+    var minutes = timer.getMinutes();
+    var seconds = timer.getSeconds();
+    if(hours < 10 && minutes<10 && seconds<10)
+    str+= `0${timer.getHours()}:0${timer.getMinutes()}:0${timer.getSeconds()}`;
+    else if(minutes < 10 && seconds<10)
+    str+= `${timer.getHours()}:0${timer.getMinutes()}:0${timer.getSeconds()}`;
+    else if(seconds<10) {
+    str+= `${timer.getHours()}:${timer.getMinutes()}:0${timer.getSeconds()}`;
+    }
+    else {
     str+= `${timer.getHours()}:${timer.getMinutes()}:${timer.getSeconds()}`;
+    }
     console.log(str);
 }
 
  setInterval(getTi,1000);
-// console.log(time);
 
-// getTi();
--- js2/task08.js
+++ js2/task08.js
@@ -1,4 +1,4 @@
-var test = 0;
+var test = 5;
 
 function toFarenheit(C) {
     var F = 1.8 * C + 32;
