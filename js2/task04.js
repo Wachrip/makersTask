fc7e140dd4eb404f83ab4f0ed7f272d2c1d650c2
Dmitriy <svardmitriy@gmail.com>
2019-07-20 10:33:37 +0600

    changed task4

--- js2/task04.js
+++ js2/task04.js
@@ -2,9 +2,18 @@ var x = 3;
 var y = 9;
 
 function myRange(x,y) {
-    for(i=x+1;i<y;i++) {
+    var tempX =0;
+    var tempY =0;
+    if(x<y) {
+        tempX = x;
+        tempY = y;
+    } else if(x>y) {
+        tempX =y;
+        tempY =x;
+    } else { console.log("числа равны");}
+    for(let i=tempX+1;i<tempY;i++) {
         console.log(i);
     }
 }
 
-myRange(x,y);
+myRange(2,10);
